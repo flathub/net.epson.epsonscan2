8d796364d450bfb0eab0d48f2e27706ae7225cec
Hubert Figuière <hub@figuiere.net>
2023-05-06 08:31:57 -0400

    Make the patching more robust

--- epson_patch.py
+++ epson_patch.py
@@ -3,8 +3,15 @@
 #
 # Patch the binary in place: it want to dlopen stuff in /usr
 #
+
+def patch(file, address):
+    exe.seek(address)
+    value = exe.read(3)
+    if value != b'usr':
+        raise Exception("incorrect file")
+    exe.seek(address)
+    exe.write(b'app')
+
 with open('/app/lib/x86_64-linux-gnu/epsonscan2/non-free-exec/es2intif', 'r+b') as exe:
-    exe.seek(0x20380 + 4)
-    exe.write(b'app')
-    exe.seek(0x20400 + 1)
-    exe.write(b'app')
+    patch(exe, 0x20380 + 4)
+    patch(exe, 0x20400 + 1)
